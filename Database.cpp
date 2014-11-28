004decd560d3e774a845e5356239c636c035f213
steinnp <steiniell@gmail.com>
2014-11-28 15:35:25 +0000

    database search functions added

--- Database.cpp
+++ Database.cpp
@@ -107,3 +107,48 @@ ostream& operator << (ostream& os, vector<Man> vm1)
     os << endl;
     return os;
 }
+
+vector<Man> Database::searchName(bool isLast, string name){
+
+    vector<Man> nameVector;
+    if(isLast){
+        for(unsigned int i = 0; i < data.size(); i++){
+            if(data[i].getLast() == name){
+                nameVector.push_back(data[i]);
+            }
+
+        }
+
+    }
+    else{
+        for(unsigned int i = 0; i < data.size(); i++){
+            if(data[i].getFirst() == name){
+                nameVector.push_back(data[i]);
+            }
+
+        }
+    }
+return nameVector;
+}
+
+vector<Man> Database::searchYear(bool isBirth, int year){
+
+    vector<Man> yearVector;
+    if(isBirth){
+        for(unsigned int i = 0; i < data.size(); i++){
+            if(data[i].getBirthYear() == year){
+                yearVector.push_back(data[i]);
+            }
+
+        }
+
+    }
+    else{
+        for(unsigned int i = 0; i < data.size(); i++){
+            if(data[i].getDeathYear() == year){
+                yearVector.push_back(data[i]);
+            }
+
+        }
+    }
+return yearVector;
